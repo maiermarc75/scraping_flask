6bd209fd59fab1f7c810bef50b9894caaeeaebb4
maiermarc <maiermarc75@gmail.com>
2022-12-01 21:35:52 +0800

    add templates and static folder for static files(css, js)

--- app.py
+++ app.py
@@ -1,17 +1,43 @@
-from flask import Flask
-from markupsafe import escape
+from flask import Flask, render_template, url_for
+
+# from markupsafe import escape
 
 app = Flask(__name__)
 
 
 @app.route("/")
-def hello_world():
-    return "<p>Hello, World!</p>"
+def index():
+    return "index"
 
 
-@app.route("/<name>")
-def hello(name):
-    return f"Hello, {escape(name)}!"
+@app.route("/login")
+def login():
+    return "login"
+
+
+@app.route("/user/<username>")
+def profile(username):
+    return f"{username}'s profile"
+
+
+# @app.route('/login', methods=['GET', 'POST'])
+# def login():
+#     if request.method == 'POST':
+#         return do_the_login()
+#     else:
+#         return show_the_login_form()
+
+with app.test_request_context():
+    print(url_for("index"))
+    print(url_for("login"))
+    print(url_for("login", next="/"))
+    print(url_for("profile", username="John Doe"))
+
+
+@app.route("/hello/")
+@app.route("/hello/<name>")
+def hello(name=None):
+    return render_template("hello.html", name=name)
 
 
 app.run(host="0.0.0.0", debug=True)
