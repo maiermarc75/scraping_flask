79b42dddfaa75740d8789b72e0627405e8a42f95
maiermarc <maiermarc75@gmail.com>
2022-12-03 11:32:53 +0800

    import  sentry and try exceptions in some case

--- app.py
+++ app.py
@@ -1,22 +1,41 @@
+import sentry_sdk
 from flask import Flask, redirect, render_template, request, url_for
-from repli360 import ScrapingEngine
+from sentry_sdk.integrations.flask import FlaskIntegration
+
+from scrape import get_scraping_data
+
+sentry_sdk.init(
+    dsn="https://a66c6ce9d612438289d0cff0d9f2efa6@o4504262732808192.ingest.sentry.io/4504262849593347",
+    integrations=[
+        FlaskIntegration(),
+    ],
+    # Set traces_sample_rate to 1.0 to capture 100%
+    # of transactions for performance monitoring.
+    # We recommend adjusting this value in production.
+    traces_sample_rate=1.0,
+)
 
 app = Flask(__name__)
 
-property_info = {}
+
+scraping_data = {}
 
 
 @app.route("/")
 def index():
-    return render_template("index.html", property_info=property_info)
+    return render_template("index.html", property_info=scraping_data)
 
 
 @app.route("/scrap", methods=["POST"])
 def scrap():
-    global property_info
+    try:
+        raise Exception("pass exception")
+    except Exception as e:
+        sentry_sdk.capture_exception(e)
+        # pass
+    global scraping_data
     _url = request.form.get("url")
-    scrapobj = ScrapingEngine()
-    property_info = scrapobj.run(_url)
+    scraping_data = get_scraping_data(_url)
     return redirect(url_for("index"))
 
 
--- scrape/__init__.py
+++ scrape/__init__.py
@@ -1,14 +1,19 @@
-import importlib
+# import importlib
 
 
-def get_scraping_data(self, data):
-    engine_mod_path = (
-        self.__module__[: self.__module__.rfind(".")]
-        + ".scrape.scraper_"
-        + data["module_path"].lower()
-    )
+# class
+def get_scraping_data(data):
+    # engine_mod_path = (
+    #     self.__module__[: self.__module__.rfind(".")]
+    #     + ".scrape.scraper_"
+    #     + data["module_path"].lower()
+    # )
 
-    engine_module = importlib.import_module(engine_mod_path)  # engine module
-    scrape_engine = engine_module.PropertyScrapingEngine(data)
-    scrape_engine.scrape()
-    return scrape_engine.get_result()
+    # engine_module = importlib.import_module(engine_mod_path)  # engine module
+    # scrape_engine = engine_module.PropertyScrapingEngine(data)
+    # scrape_engine.scrape()
+    return "scrape_engine.get_result()"
+
+
+def valid_url(_url):
+    pass
